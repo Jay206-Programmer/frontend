004b88feca71b5ced4645d92402b4c250c88cb3f
Jay Shukla <jshukla@infosenseglobal.com>
2022-03-12 13:50:23 -0800

    feat: Added Charts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import "./App.css";
 import Header from "./components/Header/header";
+import TempChart from "./components/Charts/tempChart";
+import Body from "./components/Body/body";
 
-import { useState } from 'react';
+import { useState } from "react";
 
 function App() {
-  const [darkmode, setDarkMode] = useState(true)
+  const [darkmode, setDarkMode] = useState(true);
 
   return (
-    <div className={`main ${darkmode?"dark":"light"}`}> 
-      <Header onClick={()=> setDarkMode(!darkmode)}/>
+    <div className={`main ${darkmode ? "dark" : "light"}`}>
+      <Header onClick={() => setDarkMode(!darkmode)} />
+      <Body>
+        <TempChart />
+        <TempChart />
+      </Body>
     </div>
   );
 }
